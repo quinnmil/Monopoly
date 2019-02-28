88c50f5d44d4fb8c81d3fd2ba31937efa9a33f30
masonj <masonj@uoregon.edu>
2019-02-28 15:49:33 -0800

    started implementing functions

--- game.cpp
+++ game.cpp
@@ -4,37 +4,135 @@
 #include "game.hpp"
 #include "boardSpace.hpp"
 
-//Constructor
+//8 Player Default Constructor
 Game::Game(){
     // should take number of players as an argument.
     // should create playerlist and gameSpaceList
     // gameSpaceList needs to contain 40 property entries.
 
+    //Adds properties to gameSpaceList(Instantiated in header)
+    for(int i = 0; i < 40; i++){
+        PropertyType *property;
+        gameSpaceList.push_back(property);
+    }
+
+    //Adds EIGHT players to playerList(Instantiated in header)
+    for(int i = 0; i < 8; i++){
+        PlayerType *player; //Creates new player pointer
+        playerList.push_back(player); //Adds new player to playerList
+    }
+
+    currentPlayer = playerList.at(0); //Sets current player
+    die1 = 1; //Do we need these at all?
+    die2 = 1;
+    bankMoney = 10000; //Be sure to properly set this
+}
+
+//Dynamic Overloaded Constructor
+Game::Game(int pl){
+    //Adds properties to gameSpaceList(Instantiated in header)
+    for(int i = 0; i < 40; i++){
+        PropertyType *property;
+        gameSpaceList.push_back(property);
+    }
+
+    //Adds players to playerList(Instantiated in header)
+    for(int i = 0; i < pl; i++){
+        PlayerType *player; //Creates new player object
+        playerList.push_back(player); //Adds new player to playerList
+    }
+
+    currentPlayer = playerList.at(0); //Sets current player
+    die1 = 1; //Do we need these at all?
+    die2 = 1;
+    bankMoney = 10000; //Be sure to properly set this
 }
 
 //Deconstructor
 Game::~Game(){
 
 }
-// Playerlist - list of all current players
-QList<PlayerType> Game::getPlayerList(){
 
+//********************
+//
+//GET FUNCTIONS
+//
+//********************
+
+//Gets Die Roll 1
+int Game::getDieOne(){
+    return dieRoll();
+}
+
+//Gets Die Roll 2
+int Game::getDieTwo(){
+    return dieRoll();
+}
+
+// Playerlist - list of all current players
+QList<PlayerType*> Game::getPlayerList(){
     return playerList;
 }
+
 // gameSpaceList -> list of BoardSpaces (have a list of players, and property)
 // thinking about this, we might not actually need to keep a list of players on that position. Could store property directly here...
 QList<PropertyType*> Game::getGameSpaceList(){
     return gameSpaceList;
 }
 
+//Gets name of property that current player is on
 string Game::getPropertyName(){
     // 'player' could/should be replaced with a CurrentPlayer pointer you keep in this class.
-    PlayerType player = this->getTurn(); // if you use the CurrentPlayer idea, you dont need this line.
-    int position = player.getPosition();
+    int position = currentPlayer->getPosition();
     string name = gameSpaceList[position]->getName();
     return name;
-
 }
+
+//Gets current player
+PlayerType* Game::getCurrentPlayer(){
+    return currentPlayer;
+}
+
+//Gets Index of current player
+int Game::getCurrentPlayerIndex(){
+    int currPos = 0;
+    for(int i = 0; i < playerList.length(); i++){
+        if(currentPlayer->getName().compare(playerList.at(i)->getName()) == 0){
+            currPos = i;
+        }
+    }
+    return currPos;
+}
+
+//Gets positions of all players in the board
+QList<int> Game::getAllPositions(){
+    QList<int> list; //creates list of positions to return
+    QList<PlayerType*> players = getPlayerList(); //list "players" now contains the game's full player list
+    for(int i = 0; i < players.length(); i++){
+        PlayerType *player = players.at(i); //gets player @ position in list
+        const unsigned int pos = player->getPosition(); //gets const version of position
+        list.push_back(pos); //adds to list that will be returned
+    }
+    return list;
+}
+
+//********************
+//
+//SET FUNCTIONS
+//
+//********************
+
+//Sets new current player
+void Game::incrementCurrentPlayer(){
+    int pos = getCurrentPlayerIndex();
+    currentPlayer = playerList.at(pos+1);
+}
+
+//********************
+//
+//ACCESS FUNCTIONS
+//
+//********************
 
 //New Game
 // might not need this function if the constructor does everything, your choice though.
@@ -42,20 +140,32 @@ void Game::newGame(){
 
 }
 
-//Get Turn
-// probably dont need this either if you instantiate a "currentPlayer" pointer in the constructor and increment it at each call to "endTurn()"
-int Game::getTurn(){
-	return turn;
+//End Turn
+void Game::endturn(){
+    incrementCurrentPlayer();
+    //Needs Main Window Update Call
 }
 
-//Roll Dice
-void Game::rollDice(){
-
+//Roll a die
+int Game::dieRoll(){
+    int roll;
+    roll = rand()%6+1;
+    return roll;
 }
 
 //Buy Property
 void Game::buyProperty(){
-
+    int pos = currentPlayer->getPosition();
+    PropertyType *currProp = gameSpaceList.at(pos);
+    if(currProp->getOwnedBy().compare("") == 0){
+        //IF NOT OWNED
+        if(currentPlayer->getMoney() > currProp->getCost()){
+            //IF PLAYER HAS ENOUGH MONEY
+            currentPlayer->setMoney(currentPlayer->getMoney() - currProp->getCost()); //Subtract cost from player
+            bankMoney += currProp->getCost(); //Add cost to bankMoney
+            currentPlayer->setProperty(currProp); //Give player the property
+        }
+    }
 }
 
 //Pay Rent
@@ -64,23 +174,31 @@ void Game::payRent(){
 }
 
 //Pay Player
-void Game::payPlayer(PlayerType p1, PlayerType p2){
-
+void Game::payPlayer(PlayerType p1, PlayerType p2, int value){ //p1 = who is paying, p2 = who is getting paid
+    p1.setMoney(p1.getMoney() - value);
+    if(p1.getMoney() < 0){
+        //Do they own properties?
+        //Do they want to sell them?
+        p2.setMoney(p2.getMoney() + value);
+    }
+    else{
+        p2.setMoney(p2.getMoney() + value);
+    }
 }
 
 //Start Trade
-void Game::startTrade(){
+void Game::startTrade(PlayerType p1, PlayerType p2, PropertyType property){
 
 }
 
 //Jail Player
 void Game::jailPlayer(){
-
+    currentPlayer->setJailStatus(true);
 }
 
 //Move Player
-void Game::movePlayer(){
-
+void Game::movePlayer(int die1, int die2){
+    currentPlayer->setPosition(die1, die2);
 }
 
 //Add House
--- game.hpp
+++ game.hpp
@@ -3,15 +3,17 @@
 #include <cstdlib>
 #include <string>
 #include <vector>
+#include <QApplication>
 #include "boardSpace.hpp"
 #include "player.hpp"
-#include <QApplication>
+
 
 
 class Game{
 public:
     Game(); // either use this constructor or use newGame() to create playerList and gameSpaceList
-	~Game();
+    Game(int pl); //overloaded for #players parameter
+    ~Game();
 
     // these functions are called by MainWindow::Update() and used to update the displays.
 
@@ -21,11 +23,15 @@ public:
     int getDieTwo();
 
     // These give me info relative to the position of the current player.
+    PlayerType* getCurrentPlayer(); //gets current player
+    QList<PlayerType*> getPlayerList(); //return qlist of players
     int getPlayerPosition(PlayerType player); // returns a number 0-40 that i'll use to display location on board
     string getCurrentPlayerName();  // returns name of current player "player 1" is fine for now
+    int getCurrentPlayerIndex(); //returns index of current player in playerList
     string getPropertyName();   // name of property ie: "Boardwalk"
     string getPropertyOwner();  // name of owner or "unowned"
 
+    QList<PropertyType*> getGameSpaceList(); //returns entire list of properties
     int getPropertyCost();
     int getPropertyRent();
     string getPropertyInfo(); // property description if special property, otherwise empty string is fine.
@@ -34,24 +40,24 @@ public:
     // essential game functions.
 
     void newGame();  // either use this or the constructor to create playerList and gameSpaceList
-    int getTurn();   // getting the current player is really important. I would keep a CurrentPlayer pointer. Probably rename this function. ("incrementCurrentPlayer()"?)
-    void rollDice(); // this probably shouldn't return void, maybe an int or a tuple of each roll? However you want to implement. Should also call MainWindow::update();
-    void movePlayer(); // adds roll to to previous position and calls currentplayer::setposition(int x)
+    void incrementCurrentPlayer();   // getting the current player is really important. I would keep a CurrentPlayer pointer. Probably rename this function. ("incrementCurrentPlayer()"?)
+    int dieRoll(); // this probably shouldn't return void, maybe an int or a tuple of each roll? However you want to implement. Should also call MainWindow::update();
+    void movePlayer(int die1, int die2); // adds roll to to previous position and calls currentplayer::setposition(int x)
     void endturn();  // 1. switches CurrentPlayer to next player. 2. Calls MainWindow::update() to update graphics.
 
     // I would have these next three all use a banker(p1,p2). buyProperty and payRent would just have playerX pay a "banker player" with inf money.
     void buyProperty();
 	void payRent();
-    void payPlayer(PlayerType p1, PlayerType p2);
+    void payPlayer(PlayerType p1, PlayerType p2, int value);
 
     // lower priority functions, don't worry about these for now.
-	void startTrade();
+    void startTrade(PlayerType p1, PlayerType p2, PropertyType property);
 	void jailPlayer();
 	void addHouse(BoardSpace boardSpace);
 	void addHotel(BoardSpace boardSpace);
 
 private:
-    PlayerType currentPlayer;   // this is key, as most of the functions above act relative to the currentPlayer.
+    PlayerType *currentPlayer;   // this is key, as most of the functions above act relative to the currentPlayer.
     int turn;
 	int die1, die2;
 	int bankMoney;
